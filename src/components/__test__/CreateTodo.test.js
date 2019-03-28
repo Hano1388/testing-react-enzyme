7bd9af87808c10420922e049844fa8a87fcca48f
Hano <hinsul3113@gmail.com>
2019-03-28 00:18:21 -0700

    made sure to cleanup DOM with unmount because of using Full Rendering (enzyme mount)

--- src/components/__test__/CreateTodo.test.js
+++ src/components/__test__/CreateTodo.test.js
@@ -9,6 +9,10 @@ describe('<CreateTodo />', () => {
     wrapped = mount(<CreateTodo />);
   });
 
+  afterEach( () => {
+    wrapped.unmount();
+  });
+
   it('has an input text and a button', () => {
     expect(wrapped.find('input').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
