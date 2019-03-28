049740fe52ce3a15bab50dbc65a764b27daa5832
Hano <hinsul3113@gmail.com>
2019-03-28 16:58:51 -0700

    turned provider and createStore into a reusable peace of code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import Root from 'Root';
 
 import App from 'components/App';
-import reducers from 'reducers';
-
-const store = createStore(
-  reducers,
-  {}
-);
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Root>
     <App />
-  </Provider>,
+  </Root>,
   document.querySelector('#root')
 );
