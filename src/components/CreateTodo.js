72c349b5ca60a8204a922c1f1a3e973be1a1edb6
Hano <hinsul3113@gmail.com>
2019-03-28 00:08:26 -0700

    Changed CreateTodo component to a class component and added tests for the input and button elements inside it

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,21 +1,38 @@
 import React from 'react';
 
-const CreateTodo = () => {
-  return (
-    <div className="ui container">
-      <form className="ui fluid form">
-        <div className="field">
-          <input type="text" placeholder="Todo" />
-          <div className="ui pointing red basic label">
-            Please enter a Todo
-          </div>
-          <button className="ui right floated primary button">Submit</button>
-        </div>
-        <div className="ui divider"></div>
-      </form>
+class CreateTodo extends React.Component {
+  state = { todo: '' };
 
-    </div>
-  )
+  onInputChange = (event) => {
+    this.setState({ todo: event.target.value });
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({ todo: ""});
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <form className="ui fluid form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <input
+              type="text"
+              value={this.state.todo}
+              onChange={this.onInputChange}
+            />
+            <div className="ui pointing red basic label">
+              Please enter a Todo
+            </div>
+            <button className="ui right floated primary button">Submit</button>
+          </div>
+          <div className="ui divider"></div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default CreateTodo;
---
+++ src/components/__test__/CreateTodo.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import CreateTodo from 'components/CreateTodo';
+
+describe('<CreateTodo />', () => {
+  let wrapped;
+  beforeEach( () => {
+    wrapped = mount(<CreateTodo />);
+  });
+
+  it('has an input text and a button', () => {
+    expect(wrapped.find('input').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(1);
+  })
+});
